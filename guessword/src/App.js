7cfb3bdc4a023c668b4f272e69032bcb0e2ce1ed
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-11-30 16:27:50 -0300

    logic for endgame

--- guessword/src/App.js
+++ guessword/src/App.js
@@ -15,6 +15,8 @@ const stages = [
     {id:3, name: "end"},
 ];
 
+const guessesQty = 3;
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordslist);
@@ -71,12 +73,25 @@ function App() {
         ...actualWrongLetters,
         normalizedLetter
       ]);
+      setGuesses((actualGuesses) => actualGuesses - 1);
     }
     
   };
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  }
+  useEffect(() => {
+    if(guesses <= 0){
+      clearLetterStates();
+      setGameStage(stages[2].name)
+    }
+  },[guesses])
 
   //Retry
   const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
     setGameStage(stages[0].name);
   }
 
