844639df3b54132752557b1dae90dba3cc132494
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-11-30 11:05:05 -0300

    creating app react

---
+++ guessword/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordslist = {
+    carro:["Motor","Porta","Capô", "Pneu", "Antena"],
+    fruta:["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    corpo:["Braço","Perna","Cérebro", "Pescoço", "Olhos"],
+    computador:["Mouse","Teclado","Monitor","Gabinete"],
+    programação:["Linguagem","FrameWork", "JavaScript", "React"],
+    alimento:["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
