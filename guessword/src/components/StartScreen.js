a1fff1b1d01d7742de73b1bfd50228f47b64a0cc
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2023-11-30 12:12:04 -0300

    creating startscreen

---
+++ guessword/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+    return (
+    <div className= "start">
+        <h1>Guees Word</h1>
+        <p>Clique no botão para começar a jogar</p>
+        <button>Iniciar o jogo</button>
+    </div>
+    )
+}
+
+export default StartScreen;
